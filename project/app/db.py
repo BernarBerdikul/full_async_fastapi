ad688df56c97fb44e9ac2e986e4d10d3e11ac0ba
bernarberdikul <61005409+BernarBerdikul@users.noreply.github.com>
2022-02-04 12:13:23 +0600

    Init project

---
+++ project/app/db.py
@@ -0,0 +1,30 @@
+import os
+
+from dotenv import load_dotenv
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker
+from sqlmodel import SQLModel
+
+load_dotenv()
+
+POSTGRES_USER: str = os.getenv("POSTGRES_USER")
+POSTGRES_PASSWORD: str = os.getenv("POSTGRES_PASSWORD")
+POSTGRES_DB: str = os.getenv("POSTGRES_DB")
+POSTGRES_HOST: str = os.getenv("POSTGRES_HOST")
+POSTGRES_PORT: int = int(os.getenv("POSTGRES_PORT"))
+
+DATABASE_URL: str = f"postgresql+asyncpg://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
+
+engine = create_async_engine(DATABASE_URL, echo=True, future=True)
+
+
+async def init_db():
+    async with engine.begin() as conn:
+        # await conn.run_sync(SQLModel.metadata.drop_all)
+        await conn.run_sync(SQLModel.metadata.create_all)
+
+
+async def get_session() -> AsyncSession:
+    async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+    async with async_session() as session:
+        yield session
